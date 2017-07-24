227a0797eac6cd03bd3ab364973a787126e0cd89
Deepak <deepak@arrowbits.com>
2017-07-24 09:50:18 +0530

    Testapp added

---
+++ TestApp/AbcTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AbcTableViewCell.swift
+//  TestApp
+//
+//  Created by Apple on 04/07/17.
+//  Copyright © 2017 dynamicarrowbits. All rights reserved.
+//
+
+import UIKit
+
+class AbcTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
